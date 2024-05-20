df366d464f58a6b027c78ca99429004198db0d0e
Amarildo <you@example.com>
2024-05-20 17:30:35 +0200

    added stamp

--- src/JavaShop/Cart.java
+++ src/JavaShop/Cart.java
@@ -33,6 +33,55 @@ public class Cart {
             System.out.println("Inserisci il prezzo:");
             double price = scanner.nextDouble();
             scanner.nextLine();
+
+            //scelta prodotto corrispondente
+            switch (choise) {
+                case 1:
+                    System.out.println("Inserisci l'Imei: ");
+                    String imei = scanner.nextLine();
+
+                    System.out.println("inserisci la quantità di memoria : ");
+                    int memoryLocated =scanner.nextInt();
+                    scanner.nextLine();
+
+                    cart.add(new Smartphone(code, name, description,
+                            price, iva, imei, memoryLocated));
+                    break;
+
+                case 2:
+                    System.out.println("Inserisci le dimensioni: ");
+                    String tvSize = scanner.nextLine();
+
+                    System.out.println("Smart TV (true / false) : ");
+                    boolean smartTv =scanner.nextBoolean();
+                    scanner.nextLine();
+
+                    cart.add(new Television(code, name, description,
+                            price, iva, tvSize, smartTv));
+                    break;
+
+                case 3:
+                    System.out.println("Inserisci il colore: ");
+                    String color = scanner.nextLine();
+
+                    System.out.println("Wirelles (true / false) : ");
+                    boolean wireless =scanner.nextBoolean();
+                    scanner.nextLine();
+
+                    cart.add(new Headphone(code, name, description,
+                            price, iva, color, wireless));
+                    break;
+
+                default:
+                    System.out.println("Scelta sbagliata");
+
+            }
+        }
+
+        //Stampa
+        System.out.println("Carello : ");
+        for (Prodotto prodotto : cart) {
+            System.out.println(prodotto);
         }
     }
 }
